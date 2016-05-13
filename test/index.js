3c98dd9504b24f99e8d3d439d4af347702a3f059
yongchao.qin <yongchao.qin@baidao.com>
2016-05-13 13:59:27 +0800

    add test: border

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ import webpack from 'webpack'
 import fs from 'fs'
 import path from 'path'
 import webpackConfig from './sample/webpack.config'
+import px2remWebpackPlugin from '../dist'
 
 const REGEX = /\/\*\*2w351r5q1rq2ekok\*\*\/([\s\S]*?)\/\*\*aiij34jri2rj24iji/
 const distAppFile = path.resolve(__dirname, './sample/dist/app.js')
@@ -24,7 +25,7 @@ describe('compile ./sample with my plugin', () => {
   })
 
   it('should not compile border-* property', () => {
-    expect(cssSource).toInclude('border-radius: 5px;')
+    expect(cssSource).toInclude('border-radius: 15px;')
   })
 
   it('should not compile px in MediaQuery condtion', () => {
@@ -43,4 +44,27 @@ describe('compile ./sample with my plugin', () => {
     expect(cssSource).toMatch(/left:\s+1.3866666\d+rem;/)
   })
 
+})
+
+describe('compile ./sample with my plugin using option border', () => {
+
+  let cssSource
+
+  before((done) => {
+    webpackConfig.plugins[1] = new px2remWebpackPlugin({
+      originScreenWidth: 750, border: true
+    })
+
+    webpack(webpackConfig, (err, stats) => {
+      expect(err).toNotExist()
+      const jsSource = fs.readFileSync(distAppFile, 'utf-8')
+      cssSource = jsSource.match(REGEX)[1]
+      done()
+    })
+  })
+
+  it('should compile border-* property with border option true', () => {
+    expect(cssSource).toInclude('border-radius: 0.064rem;')
+    expect(cssSource).toInclude('border: 0.128rem soild white;')
+  })
 })
